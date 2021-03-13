d38b83871fba653a0bd9689ffd3cb7b9597fa3f4
Nihararun <70094584+Nihararun@users.noreply.github.com>
2021-03-13 12:33:46 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+var canvas = document.querySelector("#myCanvas");
+var context = canvas.getContext("2d");
+
+context.beginPath();
+context.moveTo(0,300);
+context.lineTo(400,300);
+context.stroke();
+
+context.beginPath();
+context.moveTo(155,150);
+context.lineTo(220,270);
+context.lineTo(90,270);
+context.lineTo(155,150);
+context.stroke();
+
+context.beginPath();
+context.moveTo(145,110);
+context.lineTo(145,140);
+context.lineTo(110,125);
+context.closePath();
+context.stroke();
+
+context.rect(70,280,170,40);
+context.stroke();
+
+context.rect(150,100,10,180);
+context.stroke();
+
+context.beginPath();
+context.arc(330,70,45,0,2*Math.PI,true);
+context.stroke();
